a5cbc0c86e3cd4fe9f188de0fd43e036481cb7c9
Gutnar Leede <gutnar@gmail.com>
2016-11-13 22:39:41 +0200

    better igain

--- Motherboard/src/main.cpp
+++ Motherboard/src/main.cpp
@@ -36,7 +36,7 @@ PwmOut pwm0(PWM0);
 PwmOut pwm1(PWM1);
 
 void serialInterrupt();
-void parseCommad(char *command);
+void parseCommand(char *command);
 
 Ticker pidTicker;
 int pidTickerCount = 0;
@@ -46,6 +46,7 @@ char buf[32];
 int serialCount = 0;
 bool serialData = false;
 
+bool failSafeEnabled = true;
 int ticksSinceCommand = 0;
 
 bool discharging = false;
@@ -81,7 +82,6 @@ void endDischarging() {
   }
 }
 
-
 void pidTick() {
   for (int i = 0; i < NUMBER_OF_MOTORS; i++) {
     motors[i].pidTick();
@@ -91,16 +91,21 @@ void pidTick() {
     led1.setBlue(!led1.getBlue());
   }
 
-  if (ticksSinceCommand++ > 60) {
+  // Fail-safe
+  if (failSafeEnabled) {
+    ticksSinceCommand++;
+  }
+
+  if (ticksSinceCommand == 60) {
     for (int i = 0; i < NUMBER_OF_MOTORS; ++i) {
       motors[i].setSpeed(0);
     }
 
     pwm1.pulsewidth_us(100);
+  }
 
+  if (ticksSinceCommand == 180) {
     discharge();
-
-    ticksSinceCommand = 0;
   }
 }
 
@@ -109,10 +114,7 @@ int main() {
   //serial.attach(&serialInterrupt);
 
   kick = 0;
-  charge = 1;
-
-  // Start charging
-  //charge = 1;
+  charge = 0;
 
   // Dribbler motor
   pwm1.period_us(400);
@@ -127,7 +129,7 @@ int main() {
       //serial.putc(buf[serialCount]);
 
       if (buf[serialCount] == '\n') {
-        parseCommad(buf);
+        parseCommand(buf);
         serialCount = 0;
         memset(buf, 0, 32);
       } else {
@@ -154,7 +156,7 @@ void stopKick() {
   charge = 1;
 }
 
-void parseCommad(char *command) {
+void parseCommand(char *command) {
   ticksSinceCommand = 0;
 
   // command == "sd14:16:10:30"
@@ -207,7 +209,13 @@ void parseCommad(char *command) {
     charge = 0;
     kick = 1;
     kicker.attach_us(&stopKick, atoi(command+1));
-  } else if (command[0] == 'e') {
+  }
+
+  else if (command[0] == 'e') {
     discharge();
+  }
+
+  else if (command[0] == 'f') {
+    failSafeEnabled = command[1] == '1';
   }
 }
