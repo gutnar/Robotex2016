0d82d09998ee4183c2c5ed0cf7c32813690652ce
Gutnar Leede <gutnar@gmail.com>
2016-11-14 18:16:56 +0200

    correct turn direction

--- Computer/src/AI.cpp
+++ Computer/src/AI.cpp
@@ -226,9 +226,9 @@ string AI::getCommand(int dt)
                 }
                  */
 
-                if (angle < IMAGE_HALF_WIDTH && output < 0) {
+                if (mGoalCenter.x < IMAGE_HALF_WIDTH && output < 0) {
                     output *= -1;
-                } else if (angle > IMAGE_HALF_WIDTH && output > 0) {
+                } else if (mGoalCenter.x > IMAGE_HALF_WIDTH && output > 0) {
                     output *= -1;
                 }
 
